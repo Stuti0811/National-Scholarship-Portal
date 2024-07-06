73e4a7aee037522d9e4dcfcc8e735cfe718c7cc2
Gurpreetsingh241199 <gurpreet.singh241199@gmail.com>
2024-07-06 23:48:10 +0530

    Adding Homepage

---
+++ src/Components/LatestNews.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './LatestNews.css';
+
+const LatestNews = () => {
+  return (
+    <div className="latest-news">
+      <h2>Latest News</h2>
+      <ul>
+        <li>News item 1</li>
+        <li>News item 2</li>
+        <li>News item 3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default LatestNews;
---
+++ src/Components/NoticeForms.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './NoticeForms.css';
+
+const NoticeForms = () => {
+  return (
+    <div className="notice-forms">
+      <h2>Notice Forms</h2>
+      <ul>
+        <li>Notice form 1</li>
+        <li>Notice form 2</li>
+        <li>Notice form 3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default NoticeForms;
