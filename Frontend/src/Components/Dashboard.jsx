ad3eea6bf95323109a34a8f7194310464e959141
Gurpreetsingh241199 <gurpreet.singh241199@gmail.com>
2024-07-11 16:41:35 +0530

    dashboard page

---
+++ Frontend/src/Components/Dashboard.jsx
@@ -0,0 +1,19 @@
+// src/App.js
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+// import './App.css';
+
+function Dashboard() {
+  return (
+    <div className="App">
+      <Header />
+      <div className="App-content">
+        <h1>Student Dashboard</h1>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Dashboard;
