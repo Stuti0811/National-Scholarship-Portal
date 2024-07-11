dda6c82e67f29256388dc62fd414e6c454de7629
unknown <chinmaychoudhary5@gmail.com>
2024-07-11 15:53:19 +0530

    Added backend code

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           {/* <Route path="/about" element={<AboutUs />} /> */}
           <Route path="*" element={<Error />} />
